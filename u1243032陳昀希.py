2ec51e76e5f5aad34d3a0e3bc6b6d5a41a38eda9
xiaobuck0322 <tsaizongye@gmail.com>
2025-05-05 23:05:08 +0800

    Add files via upload

---
+++ u1243032陳昀希.py
@@ -0,0 +1,6 @@
+
+num = int(input("請輸入一個整數："))
+if num % 2 == 0:
+    print("這是偶數")
+else:
+    print("這是奇數")
---
+++ u1243052黃于晏.py
@@ -0,0 +1,4 @@
+
+celsius = float(input("請輸入攝氏溫度："))
+fahrenheit = celsius * 9 / 5 + 32
+print("對應的華氏溫度是：", fahrenheit)
